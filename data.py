e41f73e4b0b4ec37f5cdc88d7de36804ff889380
Alberto Repara <santillanbenitezjosealberto@gmail.com>
2025-04-04 14:53:24 -0600

    Servidor Actualizado

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-4c8de86d-bfb0-4aaa-8193-1e395ebb51fe.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-2dee7ad3-8f9a-4d87-b7ab-31fc275502cd.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
